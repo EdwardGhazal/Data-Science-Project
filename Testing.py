ced4ef5936773fdd1e145f64fcb2d1b58ac2b251
edward ghazal <edwardghazal05@gmail.com>
2025-10-05 21:59:41 +0300

    Add my new file

--- Testing.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-import pandas as pd
-print("Hey guys hows everything")
-=======
-import pandas as pd
-print("Hey guys hows everything")
->>>>>>> 58ed6f160b43229acf3746cd1ef7ecbbf451e805
